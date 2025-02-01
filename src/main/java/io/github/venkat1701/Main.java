22227743cb58694e14f9aeb4e59004696fa924a7
Venkat <thejeastdev@gmail.com>
2025-02-02 04:21:59 +0530

    v0-initialized-project

---
+++ src/main/java/io/github/venkat1701/Main.java
@@ -0,0 +1,15 @@
+package io.github.venkat1701;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
