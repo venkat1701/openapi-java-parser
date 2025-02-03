b695dab6e075691e461dfc7e6d8d9d2cc09e2fea
Venkat <thejeastdev@gmail.com>
2025-02-03 15:18:04 +0530

    vo-added-demo-testing-generating-client-sdk

--- src/main/java/io/github/venkat1701/Main.java
+++ src/main/java/io/github/venkat1701/Main.java
@@ -1,15 +1,31 @@
 package io.github.venkat1701;
 
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeSpec;
+import io.github.venkat1701.openapi.parser.YAMLParser;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.parser.core.models.ParseOptions;
 
-import javax.lang.model.element.Modifier;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+        var parser = new YAMLParser("src/main/resources/openapi.yaml");
+        var parseOptions = new ParseOptions();
+        parseOptions.setResolve(true);
+        var result = parser.readLocationWithAuthValue(null, parseOptions);
+        var openAPI = parser.getOpenAPI();
+
+        // This thing below lists down all the operations required to create the SDK.
+//        openAPI.getPaths().forEach((path, pathItem) -> {
+//            Map<PathItem.HttpMethod, Operation> operations = pathItem.readOperationsMap();
+//            operations.forEach((method, operation) -> System.out.println("Method: "+method + "\nOperation: "+operation));
+//        });
+        demoGenerateSources(parser);
+    }
+
+    public static void demoGenerateSources(YAMLParser parser) throws IOException {
+        var openApi = parser.getOpenAPI();
+
     }
 }
